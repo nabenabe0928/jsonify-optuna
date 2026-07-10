3dcb1a94e5d9d189f9143bb93c1f7db83d37ddc4
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2026-07-10 23:05:28 +0900

    Add version.py

--- jsonify_optuna/__init__.py
+++ jsonify_optuna/__init__.py
@@ -1,3 +1,4 @@
+from jsonify_optuna._version import __version__
 from jsonify_optuna.jsonify_optuna import json_to_optuna_study
 from jsonify_optuna.jsonify_optuna import jsonify
 from jsonify_optuna.jsonify_optuna import load_study
@@ -5,4 +6,4 @@ from jsonify_optuna.jsonify_optuna import StudyType
 from jsonify_optuna.jsonify_optuna import TrialType
 
 
-__all__ = ["json_to_optuna_study", "jsonify", "load_study", "StudyType", "TrialType"]
+__all__ = ["__version__", "json_to_optuna_study", "jsonify", "load_study", "StudyType", "TrialType"]
